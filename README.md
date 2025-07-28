52bd311f2da9689af0a2ce89fe647c3c4c373a4b
Thorbjørn Djupvik <thorbjorn.djupvik@gmail.com>
2025-07-28 16:59:16 +0200

    added cmdline application for ease of use

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Ugle
 Prototype of a program meant to create snapshots of multiple git repos scattered
 across the filesystem, and dependencies installed with 'Spack'
+
+## Notes
+
+# TODO: Snapshot of python packages -> Conclusion: People needs to create a
+# venv, Pipfile.lock, etc and version control these files in order to snapshot
+# the python packages.
+
+# TODO: Snapshot of javascript packages -> Conclusion: use npm-lock.json
--- src/main.py
+++ src/main.py
@@ -5,12 +5,6 @@ import argparse
 import snapshot
 import checkout
 
-# TODO: Snapshot of python packages -> Conclusion: People needs to create a
-# venv, Pipfile.lock, etc and version control these files in order to snapshot
-# the python packages.
-
-# TODO: Snapshot of javascript packages -> Conclusion: use npm-lock.json
-
 # TODO: Snapshot which spack repos packages are coming from?
 
 # TODO: Improve error handling
@@ -21,24 +15,43 @@ import checkout
 # ====================================================================================
 
 
+def checkout_handler(args) -> None:
+    checkout.checkout(args.work_dir, args.verbose)
+
+
+def snapshot_handler(args) -> None:
+    snapshot.snapshot(args.work_dir, args.verbose)
+
+
 def main():
     parser = argparse.ArgumentParser("Ugle", description="Create and retrieve snapshot")
     # work_dir is the "root" of the snapshot.
-    parser.add_argument("work_dir")
-    # subparsers = parser.add_subparsers()
-    # checkout_parser = subparsers.add_parser("checkout")
-    # snapshot_parser = subparsers.add_parser("snapshot")
+    # parser.add_argument("work_dir", help="The directory where the ugle.toml file is located.")
+    subparsers = parser.add_subparsers()
+    checkout_parser = subparsers.add_parser("checkout")
+    snapshot_parser = subparsers.add_parser("snapshot")
     #
-    # checkout_parser.add_argument("work_dir")
-    # checkout_parser.set_defaults(func=snapshot)
-    # snapshot_parser.add_argument("home")
-    # snapshot_parser.set_defaults(func=snapshot)
+    checkout_parser.add_argument(
+        "work_dir", help="The directory where the ugle.toml file is located."
+    )
+    checkout_parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Verbose printing"
+    )
+    checkout_parser.set_defaults(func=checkout_handler)
+
+    snapshot_parser.add_argument(
+        "work_dir", help="The directory where the ugle.toml file is located."
+    )
+    snapshot_parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Verbose printing"
+    )
+    snapshot_parser.set_defaults(func=snapshot_handler)
 
     args = parser.parse_args()
     # print(args.work_dir)
     # snapshot.snapshot(args.work_dir, verbose=True)
-    checkout.checkout(args.work_dir, verbose=True)
-    # args.func(args)
+    # checkout.checkout(args.work_dir, verbose=True)
+    args.func(args)
 
 
 if __name__ == "__main__":
