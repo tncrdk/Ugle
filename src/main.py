871c9cc185eb9579f1af290f139ca8ce0dc024e6
Thorbjørn Djupvik <thorbjorn.djupvik@gmail.com>
2025-07-28 13:50:16 +0200

    fixed some printing, and exception types

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import tomllib
 import json
 import os
 import shutil
-import uuid
+import datetime
 import re
 from pathlib import Path
 from typing import Optional
@@ -21,12 +21,6 @@ from typing import Optional
 # TODO: Snapshot which spack repos packages are coming from?
 
 # TODO: Improve error handling
-
-# TODO: Add the date of the snapshot to the lockfile
-
-# TODO: Consider instead of 'git checkout' the already existing repos, copy them
-# into a predetermined folder, e.g. <name>-2024-03-21, and 'git checkout'
-# there
 
 
 # ====================================================================================
@@ -67,11 +61,11 @@ def snapshot(work_dir_str: str, verbose: bool = False):
 
     # Check existence
     if not work_dir.exists():
-        raise ValueError(f"{work_dir} does not exist")
+        raise FileNotFoundError(f"{work_dir} does not exist")
     toml_file_path = work_dir / "ugle.toml"
     exists, err_msg = check_if_file_exists(toml_file_path)
     if not exists:
-        raise ValueError(err_msg)
+        raise FileNotFoundError(err_msg)
 
     with open(toml_file_path, "rb") as f:
         config = tomllib.load(f)
@@ -79,8 +73,9 @@ def snapshot(work_dir_str: str, verbose: bool = False):
     # Add the name of snapshot to the lockfile
     name = config.get("name")
     if name is None:
-        raise Exception("Name is missing from ugle.toml")
-    snapshot["name"] = name
+        raise ValueError("'name' is missing from ugle.toml")
+    date = str(datetime.date.today())
+    snapshot["name"] = name + "-" + date
 
     # If there is a Spack entry, handle it
     spack_config = config.get("spack")
@@ -118,27 +113,22 @@ def spack_deps(
     # This should exist
     lockfile_str = spack_config.get("lockfile")
     if lockfile_str is None:
-        raise ValueError(
-            f"spack has no attribute called 'lockfile' in {toml_file_path}."
-        )
+        raise KeyError(f"spack has no attribute called 'lockfile' in {toml_file_path}.")
     lockfile_path = Path(lockfile_str)
 
     # Resolves the path to an absolute path
     lockfile_path = create_absolute_path(lockfile_path, work_dir)
-    if verbose:
-        print(f"Looking for spack lockfile in: {lockfile_path}")
+    verbose_print(verbose, f"Looking for spack lockfile in: {lockfile_path}")
     exists, err_msg = check_if_file_exists(lockfile_path)
     if not exists:
-        raise ValueError(err_msg)
+        raise FileNotFoundError(err_msg)
 
-    if verbose:
-        print(f"Loading contents of {lockfile_path}")
+    verbose_print(verbose, f"Loading contents of {lockfile_path}")
     # Extract the contents of the lockfile
     with open(lockfile_path, "r") as f:
         lockfile_content = json.load(f)
 
-    if verbose:
-        print(f"Storing contents of {lockfile_path} in snapshot")
+    verbose_print(verbose, f"Storing contents of {lockfile_path} in snapshot")
     # Store the contents of the lockfile in the snapshot
     snapshot["spack"] = lockfile_content
 
@@ -159,7 +149,7 @@ def handle_other_deps(
         if filepath is not None:
             local_dep(dep_name, filepath, snapshot, work_dir, url, verbose)
         else:
-            raise ValueError(
+            raise KeyError(
                 f"The dependency {dep_name} does not supply a filepath nor an url."
             )
 
@@ -176,11 +166,10 @@ def local_dep(
     Expected to be a local git repo
     """
     filepath = create_absolute_path(Path(filepath_str), work_dir)
-    if verbose:
-        print("=" * 10)
-        print(f"Looking for {name} in {filepath}")
+    verbose_print(verbose, "=" * 10)
+    verbose_print(verbose, f"Looking for {name} in {filepath}")
     if not filepath.exists():
-        raise Exception(f"Filepath {filepath} does not exist")
+        raise FileNotFoundError(f"Filepath {filepath} does not exist")
     os.chdir(filepath)
 
     git_status = subprocess.run(["git", "status", "--porcelain"], capture_output=True)
@@ -190,10 +179,9 @@ def local_dep(
         # gave the error
         raise Exception(git_status.stderr.decode())
 
-    if verbose:
-        print("-" * 4)
-        print("Checking the working tree")
-    if not git_status.stdout.decode() == "":
+    verbose_print(verbose, "-" * 4)
+    verbose_print(verbose, "Checking the working tree")
+    if git_status.stdout.decode() != "":
         print(f"The working tree of {filepath} is not clean:")
         print(git_status.stdout.decode())
         while True:
@@ -206,29 +194,26 @@ def local_dep(
             elif ans == "n":
                 # TODO: Create better error to be handled further up the call
                 # stack
-                raise Exception("Snapshot aborted")
-            print("Not valid")
-    elif verbose:
-        print("Working tree is clean")
+                raise RuntimeError("Snapshot aborted")
 
-    if verbose:
-        print("-" * 4)
-        print("Getting commit-hash")
+            print("Not valid")
+    else:
+        verbose_print(verbose, "Working tree is clean")
+
+    verbose_print(verbose, "-" * 4)
+    verbose_print(verbose, "Getting commit-hash")
     # Get the commit-hash of the commit currently being check out
     commit_hash = (
         subprocess.run(["git", "rev-parse", "HEAD"], capture_output=True)
         .stdout.decode()
         .strip()
     )
-    if verbose:
-        print(f"Commit-hash: {commit_hash}")
+    verbose_print(verbose, f"Commit-hash: {commit_hash}")
+    verbose_print(verbose, "-" * 4)
 
-    if verbose:
-        print("-" * 4)
     # If url is not defined, try to get it from 'git remove -v'
     if url is None:
-        if verbose:
-            print("Trying to get url with 'git remote -v'")
+        verbose_print(verbose, "Trying to get url with 'git remote -v'")
 
         remote_cmd = subprocess.run(["git", "remote", "-v"], capture_output=True)
         # Regex for getting the remote url. We pick the push url since this is
@@ -242,16 +227,14 @@ def local_dep(
             # not see how multiple matches could be made.
             url = url_remote_cmd[0]
 
-            if verbose:
-                print(f"Found url: {url}")
-        elif verbose:
-            print("Did not find url")
-    elif verbose:
-        print(f"url: {url}")
+            verbose_print(verbose, f"Found url: {url}")
+        else:
+            verbose_print(verbose, "Did not find url")
+    else:
+        verbose_print(verbose, f"url: {url}")
 
-    if verbose:
-        print(f"-" * 4)
-        print(f"Adding {name} to snapshot")
+    verbose_print(verbose, f"-" * 4)
+    verbose_print(verbose, f"Adding {name} to snapshot")
 
     # If there a url exists, add it to the lockfile
     if url is not None:
@@ -281,7 +264,7 @@ def checkout(work_dir_str: str, verbose: bool = False):
 
     # Check existence
     if not work_dir.exists():
-        raise ValueError(f"{work_dir} does not exist")
+        raise FileNotFoundError(f"{work_dir} does not exist")
 
     # Create lockfile and TOML filepaths
     lockfile_path = work_dir / "ugle.lock"
@@ -291,7 +274,7 @@ def checkout(work_dir_str: str, verbose: bool = False):
     # Check if the lockfile exists
     exists, err_msg = check_if_file_exists(lockfile_path)
     if not exists:
-        raise ValueError(err_msg)
+        raise FileNotFoundError(err_msg)
     verbose_print(verbose, f"Found lockfile at {lockfile_path}")
     # Open the lockfile
     with open(lockfile_path, "r") as f:
@@ -300,11 +283,10 @@ def checkout(work_dir_str: str, verbose: bool = False):
     # Create the checkout directory where the snapshot will be rebuilt
     name: Optional[str] = config.get("name")
     if name is None:
-        raise Exception("Name is None")
+        raise ValueError("'name' not found")
     checkout_dir = (Path("~/.ugle/") / name).expanduser().resolve().absolute()
 
-    if verbose:
-        print(f"Looking for TOML-file at {tomlfile_path}")
+    verbose_print(verbose, f"Looking for TOML-file at {tomlfile_path}")
     # Check if the TOML file exists
     exists, err_msg = check_if_file_exists(tomlfile_path)
     if exists:
@@ -345,7 +327,6 @@ def checkout(work_dir_str: str, verbose: bool = False):
             # If errors occured, print them
             if err != 0:
                 print("Error: ", err)
-            print("Finished: ", " ".join(cmd))
     print("=" * 10)
     print()
 
